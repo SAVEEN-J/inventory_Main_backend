271ef0bb63f7589b4e443172538b8d98163f8ba7
SAVEEN-J <saveenj1996@gmail.com>
2024-02-16 23:08:43 +0530

    doned

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://stunning-crumble-ecf75d.netlify.app/"],
     credentials: true,
   })
 );
