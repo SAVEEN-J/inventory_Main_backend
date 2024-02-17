442cc005945be308d94f1ad0628d2eb9fd92e35d
SAVEEN-J <saveenj1996@gmail.com>
2024-02-17 12:16:47 +0530

    cred

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://inventory-main-f.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Add this line
   next();
 });
 
