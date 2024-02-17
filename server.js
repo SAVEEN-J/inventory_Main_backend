09317223bddef47e6b7996d90b63091b7e6fb6c7
SAVEEN-J <saveenj1996@gmail.com>
2024-02-17 08:42:03 +0530

    server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://stunning-crumble-ecf75d.netlify.app/"],
+    origin: ["http://localhost:3000", "https://main--inventory-main-f.netlify.app/"],
     credentials: true,
   })
 );
